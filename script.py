19f736b52ed5d8f437ffd7a018d613d830683963
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2022-02-18 18:15:11 -0500

    Intro

--- script.py
+++ script.py
@@ -8,6 +8,18 @@ class USCC:
         self.temp_history_index = 0
         self.user_display = ''
 
-    
-test = USCC
-print(test('my_face'))
+    def update_display(self, to_update):
+        self.user_display = to_update
+        print(self.user_display)
+        print("Welcome to USCC {}!".format(self.name))
+        print('-'*24)
+        print('\n')
+
+
+
+
+
+string_name = str(input('What would you like to name your calculator?: '))
+string_name = string_name.split(" ")[0]
+test = USCC(string_name)
+print(test.update_display('hi'))
